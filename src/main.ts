c3a1c1e22bcee27a5681cced61e6a544bc5c53e5
荣 <15035291867@163.com>
2023-01-11 14:03:31 +0800

    test(main): 测试一下

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-import 'dayjs/locale/zh-cn'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { createApp } from "vue"
+import ElementPlus from "element-plus"
+import zhCn from "element-plus/dist/locale/zh-cn.mjs"
+import "dayjs/locale/zh-cn"
+import * as ElementPlusIconsVue from "@element-plus/icons-vue"
 
-import 'element-plus/dist/index.css'
-import './style.css'
+import "element-plus/dist/index.css"
+import "./style.css"
 
-import router from './router'
-import App from './App.vue'
+import router from "./router"
+import App from "./App.vue"
 
-const app = createApp(App);
+const app = createApp(App)
 // 使用element icons
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(`${key}Icon`, component)
+	app.component(`${key}Icon`, component)
 }
+// 测试一下
 // 注册路由
 app.use(router)
 // 国际化
 app.use(ElementPlus, {
-    locale: zhCn,
+	locale: zhCn,
 })
 
-
-app.mount('#app')
+app.mount("#app")
